0dbc76e6d021982267ddf9b93a4c0a5740b2a3d1
Safayet Jamil <work.jamil4@gmail.com>
2021-09-24 22:29:59 +0600

    Animated Navbar

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+
+const btnHamburger= document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay=document.querySelector('.overlay');
+const fadeItems=document.querySelectorAll('.has-fade');
+btnHamburger.addEventListener("click", myFunction); 
+function myFunction() {
+    console.log ("Hello World!");
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+
+    }
+    else{
+        header.classList.add('open');
+
+        fadeItems.forEach(function(element){
+
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+
+        
+    }
+}
